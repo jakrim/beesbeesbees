ca048f1f9ce14442648d2e5397f7981da6f67aea
“Jesse <jesse.krim@gmail.com>
2019-06-03 12:35:32 -0400

    Add all methods and properties - complete Solutions

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,19 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  // inherited this.color (yellow)
+  // inherited this.food (jelly)
+  // eat method - inherited;
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+
+ForagerBee.prototype.forage = function (treasure) {
+  this.treasureChest.push(treasure);
+}
+// ForagerBee.forage('item')
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  // Color is inherited from Bee (yellow)
+  // Food is inherited
+  // Eat is inherited
+  this.honeyPot = 0;
+  
 };
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot--;
+}
 
